cd9d04c46c656e92389e9a6452a9e1fb8bd9e935
Jacob <jacobwolniak@lewisu.edu>
2021-09-30 20:23:57 -0500

    added features

--- index.js
+++ index.js
@@ -3,6 +3,6 @@ let map;
 function initMap() {
     map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 41.605, lng: -88.081},
-        zoom: 8,
+        zoom: 10,
     });
 }
