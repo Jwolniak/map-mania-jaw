9bcf15a864c358e4a2e4db91e7d549a2027071d9
Jacob <jacobwolniak@lewisu.edu>
2021-10-07 15:07:58 -0500

    editted js and onload

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function initApplication() {
 function initMap() {
     myMap = new google.maps.Map(document.getElementById("mapID"), {
         center: {lat: 41.605, lng: -88.081},
-        zoom: 13,
+        zoom: 4,
     });
 
     var marker = new google.maps.Marker({position:{lat:18.4563,lng:-77.3267}, map:myMap});
@@ -29,7 +29,7 @@ function updateGame() {
     var zoomLevel = map.getZoom();
     var inBounds= false;
 
-    if (myMap.getBounds.contains(loc1)) {
+    if (myMap.getBounds.contains(marker)) {
         inBounds = true;
         console.log("inbounds:"+inBounds+" zoomLevel:"+zoomLevel);
     }
