76d98f22c232d7ae4875ac74372eaafc925772dc
Jacob <jacobwolniak@lewisu.edu>
2021-10-07 09:16:57 -0500

    made changes to map and js

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
-let map;
+var myMap;
 
 function initMap() {
-    map = new google.maps.Map(document.getElementById("map"), {
+    myMap = new google.maps.Map(document.getElementById("mapID"), {
         center: {lat: 41.605, lng: -88.081},
-        zoom: 10,
+        zoom: 13,
     });
 
-    var marker = new google.maps.Marker({position:{lat:18.4563,lng:-77.3267}, map:map});
+    var marker = new google.maps.Marker({position:{lat:18.4563,lng:-77.3267}, map:myMap});
 
-    var marker2 = new google.maps.Marker({position:{lat:45.6366,lng:-89.4121}, map:map});
+    var marker2 = new google.maps.Marker({position:{lat:45.6366,lng:-89.4121}, map:myMap});
     marker2.setIcon('https://maps.google.com/mapfiles/kml/shapes/info-i_maps.png');
 
     var infoWindow = new google.maps.InfoWindow({content:'Rhinelander, WI'});
     marker2.addListener('click', function() {
-        infoWindow.open(map, marker2);
+        infoWindow.open(myMap, marker2);
     });
 
     google.maps.event.addListener(map, 'idle', function() {
